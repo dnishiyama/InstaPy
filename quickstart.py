795de6057cd8fed528db5b192c06d3c3c55f96e8
Declan Nishiyama <dnishiyama@gmail.com>
2018-08-17 18:18:55 -0700

    updated quickstart file for my purposes

--- quickstart.py
+++ quickstart.py
@@ -6,9 +6,6 @@ from selenium.common.exceptions import NoSuchElementException
 
 from instapy import InstaPy
 
-insta_username = ''
-insta_password = ''
-
 # set headless_browser=True if you want to run InstaPy on a server
 
 # set these in instapy/settings.py if you're locating the
@@ -16,9 +13,9 @@ insta_password = ''
 #   Settings.database_location = '/path/to/instapy.db'
 #   Settings.chromedriver_location = '/path/to/chromedriver'
 
-session = InstaPy(username=insta_username,
-                  password=insta_password,
-                  headless_browser=False,
+session = InstaPy(username=os.environ['INSTA_USERNAME'],
+                  password=os.environ['INSTA_PASSWORD'],
+                  headless_browser=True,
                   multi_logs=True)
 
 try:
@@ -26,19 +23,22 @@ try:
 
     # settings
     session.set_relationship_bounds(enabled=True,
-				 potency_ratio=-1.21,
+				 #potency_ratio=-1.21,
 				  delimit_by_numbers=True,
-				   max_followers=4590,
+				   max_followers=8500,
 				    max_following=5555,
 				     min_followers=45,
-				      min_following=77)
-    session.set_do_comment(True, percentage=10)
-    session.set_comments(['aMEIzing!', 'So much fun!!', 'Nicey!'])
-    session.set_dont_include(['friend1', 'friend2', 'friend3'])
-    session.set_dont_like(['pizza', 'girl'])
+				      min_following=56)
+    #session.set_do_comment(True, percentage=10)
+    #session.set_comments(['aMEIzing!', 'So much fun!!', 'Nicey!'])
+    #session.set_dont_include(['friend1', 'friend2', 'friend3'])
+    #session.set_dont_like(['pizza', 'girl'])
+    session.set_user_interact(amount=2, randomize=True, percentage=50, media='Photo')
+    session.set_dont_like(['watch', 'god', 'God'])      
 
     # actions
-    session.like_by_tags(['natgeo'], amount=1)
+    #session.like_by_tags(['wotd'], amount=1)
+    session.like_by_tags(['wordoftheday', 'wordfortheday', 'wordgram', 'etymology'], amount=40, interact=True)
 
 except Exception as exc:
     # if changes to IG layout, upload the file to help us locate the change
