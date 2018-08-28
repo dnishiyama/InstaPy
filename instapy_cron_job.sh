dd825c73f7912fe95a84b243c732c1c698dc27b9
Declan Nishiyama <dnishiyama@gmail.com>
2018-08-27 20:10:54 -0700

    updated script for new logs file location

--- instapy_cron_job.sh
+++ instapy_cron_job.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ubuntu/InstaPy
-/home/ubuntu/anaconda3/bin/python ./etymologyexplorer.py>/home/ubuntu/InstaPy/logs/etymologyexplorer/cron_log
+/home/ubuntu/anaconda3/bin/python ./etymologyexplorer.py>/home/ubuntu/InstaPy/logs/cron_log
